a463e89da8e79f1a2392908f1f1976606eeb864c
Daniel Galdencio <daniel.galdencio@aluno.ifsp.edu.br>
2021-04-19 09:59:48 -0300

    Criação de formulário Classifica Pessoa

---
+++ Projeto_LPRC5/classeClassificaPessoa.cs
@@ -0,0 +1,40 @@
+﻿//****************************************************************************************
+//**Criado por:
+//**Data de Criação:
+//**Instruções:
+//
+//
+//****** Atualizações:
+//*** Data:
+//*** Responsável:
+//****************************************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_LPRC5
+{
+    class classeClassificaPessoa
+    {
+        //Atributos da Classe
+        private int defaultId;
+
+        //Método Construtor
+        public classeClassificaPessoa()
+        {
+
+        }
+
+        //Métodos Acessores
+        public void setId(int id)
+        {
+            defaultId = id;
+        }
+        public int getId()
+        {
+            return defaultId;
+        }
+     } 
+}
