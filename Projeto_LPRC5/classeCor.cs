d177e8593b217bcf9224719d0104e2065e8c8840
Amanda Iaquinta <a.ferrari.iaquinta@gmail.com>
2021-05-01 17:49:49 -0300

    Criacao da classeVisitantePrestador

--- Projeto_LPRC5/classeCor.cs
+++ Projeto_LPRC5/classeCor.cs
@@ -3,10 +3,13 @@
 //**Data de Criação: 20/04/2021
 //**Instruções: Classe para auxiliar o form de Cor.
 //
-//
 //****** Atualizações: Get,set 
 //*** Data:20/04/2021
 //*** Responsável:Caique Bruno
+//
+//****** Atualizações: Criação do construtor
+//*** Data:01/05/2021
+//*** Responsável:Amanda Ferrari, André Costa, Giovanna Valim
 //****************************************************************************************
 
 using System;
@@ -22,25 +25,35 @@ namespace Projeto_LPRC5
         private int corId;
         private string corNome;
 
-            public classeCor()
-            {
+        public classeCor()
+        {
 
-            }
+        }
+
+        public classeCor(int id, string nome)
+        {
+            corId = id;
+            corNome = nome;
+        }
+
         public void setCorId(int idCor)
         {
             corId = idCor;
         }
+
         public int getCorId()
         {
             return corId;
         }
+
         public void setCorNome(string NomeCor)
         {
             corNome = NomeCor;
         }
+
         public string getCorNome()
         {
             return corNome;
         }
     }
-    }
+}
---
+++ Projeto_LPRC5/classeVisitantePrestador.cs
@@ -0,0 +1,41 @@
+﻿//****************************************************************************************
+//**Criado por: Amanda Ferrari, Giovanna Valim, André Costa
+//**Data de Criação: 01/05/2021
+//**Instruções: Criação da classe usada no form frmVisitantePrestador
+//
+//
+//****** Atualizações: 
+//*** Data:
+//*** Responsável:
+//****************************************************************************************
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_LPRC5
+{
+    public class classVisitantePrestador
+    {
+
+        private string Nome { get; set; }
+        private string Cpf { get; set; }
+        private string NomeEmpresa { get; set; }
+        private string Vinculo { get; set; }
+
+        public classVisitantePrestador()
+        {
+
+        }
+        public classVisitantePrestador(string nome, string cpf, string empresa, string vinculo)
+        {
+            this.Nome = nome;
+            this.Cpf = cpf;
+            this.NomeEmpresa = empresa;
+            this.Vinculo = vinculo;
+        }
+    }
+}
