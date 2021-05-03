633b64d1318f269586eb8daae1475611dd595dd6
Alexandro Junior <alexandro.junior@aluno.ifsp.edu.br>
2021-05-03 17:52:51 -0300

    Criaçao design do forms Moradores, incluindo informações necessarias para cadastro de morador

--- Projeto_LPRC5/Grupo 6/Moradores/frmMoradores.cs
+++ Projeto_LPRC5/Grupo 6/Moradores/frmMoradores.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿//****************************************************************************************
+//**Criado por: Alexandro Junior, Caique Bruno , Guilherme Beig
+//**Data de Criação: 03/05/2021
+//**Instruções: Criaçao design do forms Moradores, incluindo informações necessarias para cadastro de morador
+//
+//
+//****** Atualizações: 
+// *** Data:
+//*** Responsável:
+//
+//
+//
+//****** Atualizações:
+//*** Data:
+//*** Responsável:
+//****************************************************************************************
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +31,31 @@ namespace Projeto_LPRC5.Grupo_6.Moradores
         public frmMoradores()
         {
             InitializeComponent();
+        }
+
+        private void frmMoradores_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBoxdadosMorad_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButtonOutrosMorad_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
