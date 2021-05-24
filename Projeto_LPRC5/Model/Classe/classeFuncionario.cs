8c4c2abc6591b8b68a5125ce1cc1806f3b63b06a
Guilherme Rissato <52382877+GuiRissato@users.noreply.github.com>
2021-05-24 09:39:30 -0300

    classe objeto funcionario

---
+++ Projeto_LPRC5/Model/Classe/classeFuncionario.cs
@@ -0,0 +1,71 @@
+﻿//****************************************************************************************
+//**Criado por:Guilherme de Andrade Rissato
+//**Data de Criação:24/05/2021
+//**Instruções: get's e set's do objeto funcionario
+//
+//
+//****** Atualizações:
+//*** Data:
+//*** Responsável:
+//****************************************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_LPRC5.Model.Classe
+{
+    class classeFuncionario
+    {
+        private int funcionarioId;
+        private string funcionarioNome;
+        private string funcionarioSenha;
+        private int funcionarioNivelId;
+
+        public classeFuncionario(int funcionarioId, string funcionarioNome, string funcionarioSenha, int funcionarioNivelId)
+        {
+            this.funcionarioId = funcionarioId;
+            this.funcionarioNome = funcionarioNome;
+            this.funcionarioSenha = funcionarioSenha;
+            this.funcionarioNivelId = funcionarioNivelId;
+        }
+
+        public void setFuncionarioId(int id) {
+            funcionarioId = id;
+        }
+
+        public int getFuncionarioId() {
+            return funcionarioId;
+        }
+
+        public void setFuncionarioNome(string nome)
+        {
+            funcionarioNome = nome;
+        }
+
+        public string getFuncionarioNome()
+        {
+            return funcionarioNome;
+        }
+
+        public void setFuncionarioSenha(string senha)
+        {
+            funcionarioSenha = senha;
+        }
+
+        public string getFuncionarioSenha()
+        {
+            return funcionarioSenha;
+        }
+        public void setFuncionarioNivelId(int nivelId)
+        {
+            funcionarioNivelId = nivelId;
+        }
+
+        public int getFuncionarioNivelId()
+        {
+            return funcionarioNivelId;
+        }
+    }
+}
