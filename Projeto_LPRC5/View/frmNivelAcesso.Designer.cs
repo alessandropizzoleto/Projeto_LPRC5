63a823f2856b954d052436d858e59266ed237f84
antonio naranjo <totyjnc@gmail.com>
2021-05-30 16:06:48 -0300

    Criação de formulário, Implementação das classes e funções junto a linkagem dos dbs e classe.

---
+++ Projeto_LPRC5/View/frmNivelAcesso.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace Projeto_LPRC5.View
+{
+    partial class frmNivelAcesso
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmNivelAcesso));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnNovo = new System.Windows.Forms.ToolStripButton();
+            this.btnEditar = new System.Windows.Forms.ToolStripButton();
+            this.btnExcluir = new System.Windows.Forms.ToolStripButton();
+            this.btnSalvar = new System.Windows.Forms.ToolStripButton();
+            this.btnCancelar = new System.Windows.Forms.ToolStripButton();
+            this.btnSair = new System.Windows.Forms.ToolStripButton();
+            this.grdDadosCid = new System.Windows.Forms.DataGridView();
+            this.txtAcesso = new System.Windows.Forms.TextBox();
+            this.lblDescricao = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdDadosCid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnNovo,
+            this.btnEditar,
+            this.btnExcluir,
+            this.btnSalvar,
+            this.btnCancelar,
+            this.btnSair});
+            this.toolStrip1.Location = new System.Drawing.Point(9, 9);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(228, 39);
+            this.toolStrip1.TabIndex = 6;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnNovo.Image = ((System.Drawing.Image)(resources.GetObject("btnNovo.Image")));
+            this.btnNovo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(36, 36);
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.ToolTipText = "Adicionar Cidade";
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnEditar.Image = ((System.Drawing.Image)(resources.GetObject("btnEditar.Image")));
+            this.btnEditar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(36, 36);
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.ToolTipText = "Editar Cidade";
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnExcluir.Image = ((System.Drawing.Image)(resources.GetObject("btnExcluir.Image")));
+            this.btnExcluir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(36, 36);
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.ToolTipText = "Excluir Cidade";
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnSalvar.Image = ((System.Drawing.Image)(resources.GetObject("btnSalvar.Image")));
+            this.btnSalvar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(36, 36);
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.ToolTipText = "Salvar nova Cidade";
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnCancelar.Image = ((System.Drawing.Image)(resources.GetObject("btnCancelar.Image")));
+            this.btnCancelar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(36, 36);
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.ToolTipText = "Cancela a operação de conclusão ou edição";
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnSair.Image = ((System.Drawing.Image)(resources.GetObject("btnSair.Image")));
+            this.btnSair.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(36, 36);
+            this.btnSair.Text = "Fechar";
+            this.btnSair.ToolTipText = "Fechar Manutenção de Cidade";
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // grdDadosCid
+            // 
+            this.grdDadosCid.AllowUserToAddRows = false;
+            this.grdDadosCid.AllowUserToDeleteRows = false;
+            this.grdDadosCid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdDadosCid.Location = new System.Drawing.Point(12, 203);
+            this.grdDadosCid.Name = "grdDadosCid";
+            this.grdDadosCid.ReadOnly = true;
+            this.grdDadosCid.RowHeadersWidth = 51;
+            this.grdDadosCid.Size = new System.Drawing.Size(798, 289);
+            this.grdDadosCid.TabIndex = 7;
+            this.grdDadosCid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdDadosCid_CellContentClick);
+            // 
+            // txtAcesso
+            // 
+            this.txtAcesso.Location = new System.Drawing.Point(15, 160);
+            this.txtAcesso.Name = "txtAcesso";
+            this.txtAcesso.Size = new System.Drawing.Size(286, 20);
+            this.txtAcesso.TabIndex = 9;
+            this.txtAcesso.TextChanged += new System.EventHandler(this.txtCidade_TextChanged);
+            // 
+            // lblDescricao
+            // 
+            this.lblDescricao.AutoSize = true;
+            this.lblDescricao.Location = new System.Drawing.Point(12, 134);
+            this.lblDescricao.Name = "lblDescricao";
+            this.lblDescricao.Size = new System.Drawing.Size(55, 13);
+            this.lblDescricao.TabIndex = 8;
+            this.lblDescricao.Text = "Descricao";
+            this.lblDescricao.Click += new System.EventHandler(this.lblCidade_Click);
+            // 
+            // frmNivelAcesso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 613);
+            this.Controls.Add(this.txtAcesso);
+            this.Controls.Add(this.lblDescricao);
+            this.Controls.Add(this.grdDadosCid);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "frmNivelAcesso";
+            this.Text = "frmNivelAcesso";
+            this.Load += new System.EventHandler(this.frmNivelAcesso_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdDadosCid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnNovo;
+        private System.Windows.Forms.ToolStripButton btnEditar;
+        private System.Windows.Forms.ToolStripButton btnExcluir;
+        private System.Windows.Forms.ToolStripButton btnSalvar;
+        private System.Windows.Forms.ToolStripButton btnCancelar;
+        private System.Windows.Forms.ToolStripButton btnSair;
+        private System.Windows.Forms.DataGridView grdDadosCid;
+        private System.Windows.Forms.TextBox txtAcesso;
+        private System.Windows.Forms.Label lblDescricao;
+    }
+}
---
+++ Projeto_LPRC5/View/frmNivelAcesso.cs
@@ -0,0 +1,233 @@
+﻿//****************************************************************************************
+//**Criado por: Antonio Naranjo  e Rafael Fogaça
+//**Data de Criação: 30/05/2021
+//**Instruções: Criação de formulário
+//****************************************************************************************
+
+using Projeto_LPRC5.Model.Classe;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Projeto_LPRC5.View {
+    public partial class frmNivelAcesso : Form
+    {
+        public frmNivelAcesso()
+        {
+            InitializeComponent();
+        }
+
+        dbNivelAcesso db_NivelAcesso = new dbNivelAcesso();
+        classeNivelAcesso classeNivelAcesso = new classeNivelAcesso();
+
+        private void formataGrid()
+        {
+            //Opção para selecionar a linha inteira do grid
+            grdDadosCid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            grdDadosCid.Columns[0].HeaderText = "Código";
+            grdDadosCid.Columns[1].HeaderText = "Nome";
+
+            grdDadosCid.Columns[0].Width = 0;
+            grdDadosCid.Columns[1].Width = 120;
+
+        }
+        private void limpaCamposDados()
+        {
+            txtAcesso.Text = "";
+            classeNivelAcesso.Id = -1;
+        }
+
+        private bool verificaDadosnotnull()
+        {
+            bool resultado = true;
+            // habilitacao da verificao
+            if (txtAcesso.Text.Trim().Length < 2)
+            {
+                resultado = false;
+            }
+
+            return resultado;
+        }
+
+        private void atualizaDadosControles()
+        {
+            //verificação se o objeto nao é nulo
+            classeNivelAcesso = db_NivelAcesso.RetornaDadosObjeto(classeNivelAcesso);
+
+            txtAcesso.Text = classeNivelAcesso.Descricao;
+        }
+
+
+        private void excluiNivelAcesso()
+        {
+            if (classeNivelAcesso.Id != 0)
+            {
+                DialogResult retorno = MessageBox.Show("Deseja excluir a informação selecionada ?", "Aviso!!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (retorno == DialogResult.Yes)
+                {
+                    db_NivelAcesso.excluiNivelAcessoBase(classeNivelAcesso);
+
+                    limpaCamposDados();
+                    atualizaDadosGrid();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Não há informação selecionada para excluir!!", "Aviso!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        private void salvaNivelAcesso()
+        {
+            if (verificaDadosnotnull() == true)
+            {
+                //Atualizando os dados do objeto cidade.
+                classeNivelAcesso.Descricao = txtAcesso.Text;
+
+                if (classeNivelAcesso.Id == -1)
+                {
+                    //Insere os dados
+                    //alimentacao da classe objeto com a informacao do txtCidade para ser enviado ao banco de dados
+                    classeNivelAcesso.Descricao = txtAcesso.Text;
+                    db_NivelAcesso.insereNivelAcessoBase(classeNivelAcesso);
+                }
+                else
+                {
+                    //Altera os dados
+                    //alimentacao da classe objeto com a informacao do txtCidade para ser enviado ao banco de dados
+                    classeNivelAcesso.Descricao = txtAcesso.Text;
+                    db_NivelAcesso.alteraNivelAcessoBase(classeNivelAcesso);
+                }
+                habilitaBotoesMenu(true);
+                habilitaCamposDados(false);
+                limpaCamposDados();
+                atualizaDadosGrid();
+            }
+            else
+            {
+                MessageBox.Show("Dados Obrigatórios não informados ", "Urgente!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public void atualizaDadosGrid()
+        {
+            grdDadosCid.DataSource = db_NivelAcesso.selectNivelAcessoBase();
+        }
+
+        private void habilitaBotoesMenu(bool hablitar)
+        {
+            btnNovo.Enabled = hablitar;
+            btnEditar.Enabled = hablitar;
+            btnExcluir.Enabled = hablitar;
+            btnSalvar.Enabled = !hablitar;
+            btnCancelar.Enabled = !hablitar;
+            btnSair.Enabled = hablitar;
+        }
+        private void alteraNivelAcesso()
+        {
+            habilitaBotoesMenu(false);
+            habilitaCamposDados(true);
+        }
+
+        private void habilitaCamposDados(bool habilitar)
+        {
+            //habilitação do campo txtCidade e o grd no form
+            txtAcesso.Enabled = habilitar;
+            grdDadosCid.Enabled = !habilitar;
+        }
+
+
+
+        private void cancelaAcesso()
+        {
+            DialogResult retorno = MessageBox.Show("Deseja cancelar o Cadastro/Atualização do Acesso?", "Aviso!!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (retorno == DialogResult.Yes)
+            {
+                habilitaBotoesMenu(true);
+                habilitaCamposDados(false);
+                limpaCamposDados();
+            }
+        }
+        private void fechaAcesso()
+        {
+            this.Close();
+        }
+
+        private void frmNivelAcesso_Load(object sender, EventArgs e)
+        {
+            habilitaBotoesMenu(true);
+            habilitaCamposDados(false);
+            atualizaDadosGrid();
+            formataGrid();
+        }
+
+        private void grdDadosCid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void insereNivelAcesso()
+        {
+            habilitaBotoesMenu(false);
+            habilitaCamposDados(true);
+            limpaCamposDados();
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            insereNivelAcesso();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            alteraNivelAcesso();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            excluiNivelAcesso();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            salvaNivelAcesso();
+        }
+
+        private void txtCidade_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            cancelaAcesso();
+        }
+
+        private void lblCidade_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            fechaAcesso();
+        }
+        private void grdDadosCid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            classeNivelAcesso.Id=(Convert.ToInt16(grdDadosCid.Rows[grdDadosCid.CurrentRow.Index].Cells[0].Value.ToString()));
+            atualizaDadosControles();
+        }
+    }
+}
+
